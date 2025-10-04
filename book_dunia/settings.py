956c6700a311611e3911b6739ddf68a74f93b2ef
Aditya Alluri <your@email.com>
2025-10-05 02:13:35 +0530

    Fix static paths and add collected files

--- book_dunia/settings.py
+++ book_dunia/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,6 +42,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -116,8 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'book_dunia/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Razorpay API Keys
 RAZORPAY_KEY_ID = 'rzp_test_RPSgTA3U8Ze1Q8'
